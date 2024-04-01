daa62d989a423adc6538bef643d83ee84c5c1e54
logicallaw <logicallawbio@gmail.com>
2024-04-01 22:37:36 +0900

    Update:README.md

---
+++ README.md
@@ -0,0 +1,13 @@
+nodeProject
+=============
+Environment
+
+MacOS M1 16GB
+Express.js
+Visual Studio Code
+-------------
+How to start server?
+
+First : Move to root directory and open a terminal.
+Second : Input 'npm start'.
+Third : Go to 'http://localhost:8001'
--- routers/router.js
+++ routers/router.js
@@ -1,6 +1,5 @@
 const express = require('express'); //express:클래스
 const router = express.Router(); //.Router()는 static 메서드함수(객체 없어도 호출가능)
-const path = require('path');
 const craw = require('./craw.js');
 
 //nunjucks로 변경
@@ -11,5 +10,6 @@ router.get('/', async (req,res,next)=>{
             titles : arrayTitles,
         })
 })
+
 
 module.exports = router; //router객체를 내보냅니다.
